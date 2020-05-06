03c636d9c28311bd4209083b054f27b3c3157dd9
voidsatisfaction <lourie@naver.com>
2020-05-06 23:59:33 +0900

    leetcode_prac: add solution of 148

--- 148_medium_[sort]_sort_list.py
+++ 148_medium_[sort]_sort_list.py
@@ -75,6 +75,29 @@ class Solution:
 
         return sort_list_rec(head, right_node)
 
+class Solution(object):
+    def merge(self, h1, h2):
+        dummy = tail = ListNode(None)
+        while h1 and h2:
+            if h1.val < h2.val:
+                tail.next, tail, h1 = h1, h1, h1.next
+            else:
+                tail.next, tail, h2 = h2, h2, h2.next
+    
+        tail.next = h1 or h2
+        return dummy.next
+    
+    def sortList(self, head):
+        if not head or not head.next:
+            return head
+    
+        pre, slow, fast = None, head, head
+        while fast and fast.next:
+            pre, slow, fast = slow, slow.next, fast.next.next
+        pre.next = None
+
+        return self.merge(*map(self.sortList, (head, slow)))
+
 if __name__ == '__main__':
     # linked_list = ListNode(4, ListNode(2, ListNode(1, ListNode(3))))
     linked_list = ListNode(-1, ListNode(5, ListNode(3, ListNode(4, ListNode(0)))))
