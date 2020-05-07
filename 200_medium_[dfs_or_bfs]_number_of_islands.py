c443f10540a109626e21b21dd0564021347f63be
voidSatisfaction <lourie@naver.com>
2020-05-07 20:25:44 +0900

    leetcode_pac: add solution of 200

--- 200_medium_[dfs_or_bfs]_number_of_islands.py
+++ 200_medium_[dfs_or_bfs]_number_of_islands.py
@@ -1,10 +1,10 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(i: int, j: int, visited: List[List[bool]]) -> None:
-            if i < 0 or j < 0 or i >= N or j >= M or grid[i][j] == "0" or visited[i][j]:
+        def dfs(i: int, j: int) -> None:
+            if i < 0 or j < 0 or i >= N or j >= M or grid[i][j] == "0":
                 return
 
-            visited[i][j] = True
+            grid[i][j] = "0"
 
             positions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
@@ -12,22 +12,20 @@ class Solution:
                 neighbor_i = i + pos[0]
                 neighbor_j = j + pos[1]
 
-                dfs(neighbor_i, neighbor_j, visited)
+                dfs(neighbor_i, neighbor_j)
 
         if len(grid) == 0 or len(grid[0]) == 0:
             return 0
 
         N, M = len(grid), len(grid[0])
 
-        visited = [ [ False for _ in range(M) ] for _ in range(N) ]
-
         island_num = 0
 
         for i in range(N):
             for j in range(M):
-                if grid[i][j] == "1" and not visited[i][j]:
+                if grid[i][j] == "1":
                     island_num += 1
-                    dfs(i, j, visited)
+                    dfs(i, j)
 
         return island_num
 
