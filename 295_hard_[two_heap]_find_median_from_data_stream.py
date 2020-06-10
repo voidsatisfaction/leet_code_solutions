ab9a3ad2cac48cef7eb3b357a0e47002e0acaae6
voidSatisfaction <lourie@naver.com>
2020-06-10 20:38:51 +0900

    leetcode_prac: add soltuion of 295

---
+++ 295_hard_[two_heap]_find_median_from_data_stream.py
@@ -0,0 +1,59 @@
+from heapq import heappush, heappop
+
+class MedianFinder:
+    def __init__(self):
+        self._max_heap = []
+        self._min_heap = []
+        self._is_even = True
+        
+    def addNum(self, num: int) -> None:
+        if len(self._max_heap) == 0:
+            heappush(self._max_heap, -num)
+            self._is_even = not self._is_even
+            return
+        
+        max_heap_top = heappop(self._max_heap)
+
+        if num <= -max_heap_top:
+            heappush(self._max_heap, max_heap_top)
+            heappush(self._max_heap, -num)
+            if len(self._max_heap) - len(self._min_heap) == 2:
+                max_heap_top = heappop(self._max_heap)
+                heappush(self._min_heap, -max_heap_top)
+        else:
+            heappush(self._max_heap, max_heap_top)
+            heappush(self._min_heap, num)
+            if len(self._min_heap) - len(self._max_heap) == 1:
+                min_heap_top = heappop(self._min_heap)
+                heappush(self._max_heap, -min_heap_top)
+
+        self._is_even = not self._is_even
+            
+
+    def findMedian(self) -> float:
+        if len(self._max_heap) == 0:
+            return 0
+
+        if self._is_even is True:
+            max_heap_top = heappop(self._max_heap)
+            min_heap_top = heappop(self._min_heap)
+
+            heappush(self._max_heap, max_heap_top)
+            heappush(self._min_heap, min_heap_top)
+
+            return (-max_heap_top + min_heap_top) / 2
+        else:
+            max_heap_top = heappop(self._max_heap)
+
+            heappush(self._max_heap, max_heap_top)
+
+            return -max_heap_top
+
+
+if __name__ == '__main__':
+    mf = MedianFinder()
+    mf.addNum(1)
+    mf.addNum(2)
+    assert mf.findMedian() == 1.5
+    mf.addNum(3) 
+    assert mf.findMedian() == 2
